dfc830f3996f2ee74a6dfe80696596448d41f308
Biagio Nuzzo <57946383+biagio-nuzzo@users.noreply.github.com>
2022-09-16 13:38:23 +0400

    fixed index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import useAuth from "./src/Hooks/useAuth";
-
-import AuthProvider from "./src/Components/AuthProvider";
-import AuthRoute from "./src/Components/AuthRoute";
-
-export default useAuth;
-
-export { AuthProvider, AuthRoute };
