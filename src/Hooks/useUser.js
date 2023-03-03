7576f9165feaea16b9adaab84cb8c01ce336d450
riccardotornesello <riccardo.tornesello@gmail.com>
2023-03-03 14:39:25 +0400

    refresh user info in useuser

--- src/Hooks/useUser.js
+++ src/Hooks/useUser.js
@@ -1,12 +1,34 @@
 // React
 import React, { useContext } from 'react';
 
+// Hooks
+import useQuery from '@hybris-software/use-query/dist/Hooks/useQuery';
+
 // Contexts
 import UserInfoContext from '../Context/UserInfoContext';
+import AuthProviderContext from '../Context/AuthProviderContext';
 
 const useUser = () => {
-  const { userInfo } = useContext(UserInfoContext);
-  return { userInfo };
+  const { userInfo, setUserInfo } = useContext(UserInfoContext);
+  const authUrl = useContext(AuthProviderContext);
+
+  const userInfoQuery = useQuery({
+    url: authUrl,
+    method: 'GET',
+    executeImmediately: false,
+    onSuccess: (response) => {
+      setUserInfo(response.data);
+    },
+    onUnauthorized: (error) => {
+      setUserInfo({});
+    },
+  });
+
+  const refreshUserInfo = () => {
+    userInfoQuery.executeQuery();
+  };
+
+  return { userInfo, refreshUserInfo };
 };
 
 export default useUser;
